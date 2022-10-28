2b1dabdb0b2e00b7bb4716b1760c2a9ed17153ff
Adriana sm <a.s.m27267@gmail.com>
2022-10-27 21:00:47 -0300

    conectando com backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("https://books.ronier.me")
+      .then((response) => response.json())
+      .then((data) => setBooks(data.content));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table border="1">
+        <thead>
+          <td>Titulo</td>
+          <td>Autor</td>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr>
+              <td>{book.title} </td>
+              <td>{book.authors} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
